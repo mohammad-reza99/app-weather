85de6d39b311a5bdbfa2b3c731fa1603799ddfb0
Mohammad Reza Zaeri <rezazaeri1990@gmail.com>
2025-11-08 22:30:54 +0100

    adding a search bar to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./components/Header";
 import "./App.css";
 import { WeatherProvider } from "./context/WeatherContext";
 import { fonts, colors } from "./styles/styleGuide";
+import SearchBar from "./components/SearchBar";
 
 function App() {
   return (
@@ -16,6 +17,12 @@ function App() {
           }}
         >
           <Header />
+
+          <main className="w-full  grid grid-cols-1 md:grid-cols-[2fr-1fr] gap 10 mt-8 px-6">
+            <div className="flex flex-col justify-center items-center w-full">
+              <SearchBar />
+            </div>
+          </main>
         </div>
       </WeatherProvider>
     </>
