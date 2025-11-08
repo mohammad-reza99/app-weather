3d36addbc9b051e7ad70a02ed67e9e513867b0aa
Mohammad Reza Zaeri <rezazaeri1990@gmail.com>
2025-11-08 18:30:06 +0100

    setting up weather app & component Weathercontext

---
+++ src/context/WeatherContext.jsx
@@ -0,0 +1,63 @@
+import { Children, createContext, useState } from "react";
+import axios from "axios";
+import { use } from "react";
+
+export const WeatherContext = createContext();
+
+export const WeatherProvider = ({ Children }) => {
+  const [weatherData, setWeatherData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const [tempUnit, setTempUnit] = useState("celsius");
+  const [windUnit, setWindUnit] = useState("kmh");
+
+  const fetchWeather = async (cityName) => {
+    try {
+      setLoading(true);
+      setError("");
+      setWeatherData(null);
+
+      const geoRes = await axios.get(
+        `https://geocoding-api.open-meteo.com/v1/search?name=${cityName.trim()}&count=1&language=en`
+      );
+      if (!geoRes.data.results || geoRes.data.results.length === 0) {
+        setError("City not found");
+        return;
+      }
+      const { latitude, longitude, name, country } = geoRes.data.results[0];
+
+      const weatherRes = await axios.get(
+        `https://api.open-meteo.com/v1/gfs?latitude=${latitude}&longitude=${longitude}&current_weather=true&hourly=temperature_2m,relative_humidity_2m,precipitation,wind_speed_10m&daily=temperature_2m_max,temperature_2m_min,precipitation_sum&timezone=auto`
+      );
+      if (!weatherRes.data.current_weather) {
+        setError("No weather data avalible for this city");
+        return;
+      }
+      setWeatherData({
+        city: name,
+        country,
+        current: weatherRes.data.courent_weather,
+        houly: weatherRes.data.hourly,
+        daily: weatherRes.data.daily,
+      });
+    } catch (err) {
+      console.error("Weather API error", err);
+      setError("Faild to fetch weather data");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const convertTemperature = (temp) => {
+    if (temp == null) return 0;
+    return tempUnit === "celsius" ? temp : (temp * 9) / 5 + 32;
+  };
+
+  const convertWindSpeed = (speed) => {
+    if (speed == null) return 0;
+    return windUnit === "kmh" ? speed : speed / 1.609;
+  };
+};
+
+export default WeatherContext;
---
+++ src/styles/styleGuide.js
@@ -0,0 +1,37 @@
+export const colors = {
+  neutral: {
+    900: "hsl(243, 96%, 9%)",
+    800: "hsl(243, 27%, 20%)",
+    700: "hsl(243, 23%, 24%)",
+    600: "hsl(243, 23%, 30%)",
+    300: "hsl(240, 6%, 70%)",
+    200: "hsl(250, 6%, 84%)",
+    0: "hsl(0, 0%, 100%)",
+  },
+  orange: {
+    500: "hsl(28, 100%, 52%)",
+  },
+  blue: {
+    500: "hsl(233, 67%, 56%)",
+    700: "hsl(248, 70%, 36%)",
+  },
+};
+
+export const fonts = {
+  body: '"DM Sans", sans-serif',
+  display: '"Bricolage Grotesque", sans-serif',
+  baseSize: "18px",
+};
+
+export const layout = {
+  mobile: "375px",
+  desktop: "1440px",
+};
+
+const styleGuide = {
+  colors,
+  fonts,
+  layout,
+};
+
+export default styleGuide;
